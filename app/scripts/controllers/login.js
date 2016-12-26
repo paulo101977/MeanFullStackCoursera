6d21c82e019b439b474013c16731a4f3d6017c5b
paulo101977 <paulo10.1977@yahoo.com.br>
2016-12-26 21:22:26 -0200

    Update video page

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -4,6 +4,7 @@ app
   .controller('LoginCTRL', ['$scope' , '$uibModalInstance' , function ($scope , $uibModalInstance) {
     $scope.submit = function(){
         $uibModalInstance.dismiss();
+        console.log('')
     }
     
     $scope.cancel = function(){
